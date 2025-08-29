d6df674e8cb1c959a0d5adbff0eef9075457ec95
Kimberly <kkristy1218@gmail.com>
2025-08-29 11:20:04 -0300

    criando as rotas do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./routes/Home";
+import Error from "./routes/Error";
+import Cliente from "./routes/Cliente";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    // habilita a navegação por rotas na aplicação
+    <BrowserRouter>
+      {/* chama o componente */}
+      <Nav />
+      <main>
+        {/* gerencia exibição om base na url */}
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cliente" element={<Cliente />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
