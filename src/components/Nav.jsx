ee1dc83566309b4c0d3c605b852e208bd75724cc
Kimberly <kkristy1218@gmail.com>
2025-08-29 11:23:56 -0300

    criando o componente nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,21 @@
-
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header>
+      <nav>
+        <img src="" alt="logo" />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cliente">Cliente</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
